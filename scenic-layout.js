4b21c2b3796c9bf1097441028ff4222083ceac2e
Andrew GitHub <langmaid.andrew@gmail.com>
2026-08-18 07:12:36 +0200

    Show all scenic paintings in overview

--- scenic-layout.js
+++ scenic-layout.js
@@ -1,60 +1,64 @@
 window.SCENIC_LAYOUT = {
-  version: 'locked-2026-08-17',
+  version: 'locked-2026-08-18-all-days',
   cards: {
     'haverud-highlight': {
       asset: 'scenic-art/haverud-aqueduct.webp',
       overview: true,
-      overviewOffset: [-245, 52],
+      overviewOffset: [-260, 55],
       dayOffset: [-190, 34]
     },
     'glava-highlight': {
       asset: 'scenic-art/glava-glasbruk.webp',
       overview: true,
-      overviewOffset: [-258, -36],
+      overviewOffset: [-265, -28],
       dayOffset: [-205, -24]
     },
     'glaskogen': {
       asset: 'scenic-art/glaskogen-nature-reserve.webp',
-      overview: false,
+      overview: true,
+      overviewOffset: [-270, 78],
       dayOffset: [-230, 105]
     },
     'fryksdalen': {
       asset: 'scenic-art/fryksdalen.webp',
-      overview: false,
+      overview: true,
+      overviewOffset: [-255, 32],
       dayOffset: [-218, 22]
     },
     'tossebergsklatten': {
       asset: 'scenic-art/tossebergsklatten.webp',
-      overview: false,
+      overview: true,
+      overviewOffset: [-260, -82],
       dayOffset: [-225, -92]
     },
     'ritamaki-highlight': {
       asset: 'scenic-art/ritamaki-finngard.webp',
       overview: true,
-      overviewOffset: [-270, -132],
+      overviewOffset: [-285, -138],
       dayOffset: [-225, -125]
     },
     'hovfjallet': {
       asset: 'scenic-art/hovfjallet.webp',
       overview: true,
-      overviewOffset: [112, -145],
+      overviewOffset: [122, -150],
       dayOffset: [100, -128]
     },
     'klaralvdalen': {
       asset: 'scenic-art/klaralvdalen.webp',
-      overview: false,
+      overview: true,
+      overviewOffset: [138, -18],
       dayOffset: [128, -24]
     },
     'vadstena-highlight': {
       asset: 'scenic-art/vadstena.webp',
       overview: true,
-      overviewOffset: [158, -76],
+      overviewOffset: [165, -82],
       dayOffset: [132, -65]
     },
     'granna': {
       asset: 'scenic-art/granna.webp',
       overview: true,
-      overviewOffset: [155, 42],
+      overviewOffset: [160, 46],
       dayOffset: [132, 36]
     }
   }
